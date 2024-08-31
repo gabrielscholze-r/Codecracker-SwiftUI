2dc4d1264a53ade62ed21cd1cc8982217bf3e5d1
gabrielscholze-r <gabrielscholze24@gmail.com>
2024-08-31 07:39:50 -0400

    Initial Commit

---
+++ codecracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  codecracker
+//
+//  Created by user264588 on 8/31/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ codecracker/codecrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  codecrackerApp.swift
+//  codecracker
+//
+//  Created by user264588 on 8/31/24.
+//
+
+import SwiftUI
+
+@main
+struct codecrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
